8eb8e82991a3f5ac390a54f4ace8e10b63d1f176
NizarZouabi <136129653+NizarZouabi@users.noreply.github.com>
2024-02-11 14:11:37 +0100

    #

---
+++ Spring/Practice/mvc/src/main/java/com/assignment/mvc/controllers/BooksController.java
@@ -0,0 +1,24 @@
+package com.assignment.mvc.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.assignment.mvc.models.Book;
+import com.assignment.mvc.services.BookService;
+
+@Controller
+public class BooksController {
+	@Autowired
+	BookService bookService;
+	
+	@GetMapping("/books/{id}")
+	public String index(Model model, @PathVariable("id") Long id) {
+		Book book = bookService.findBook(id);
+		model.addAttribute("book", book);
+		
+		return "show.jsp";
+	}
+}
